02be45bb38718cf28c01db936832aac56243b101
GuzlejM <martin.guzlej@easysoftware.com>
2025-01-18 13:02:09 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,37 +1,12 @@
-import config from "@config/config.json";
-import Cta from "@layouts/components/Cta";
-import SeoMeta from "@layouts/SeoMeta";
-
-import HomeBanner from "@layouts/partials/HomeBanner";
-import HomeFeatures from "@layouts/partials/HomeFeatures";
-import Services from "@layouts/partials/Services";
-import Workflow from "@layouts/partials/Workflow";
-import { getListPage } from "../lib/contentParser";
+import AgentChat from "@layouts/AgentChat";
+import { getListPage, getRegularPage } from "../lib/contentParser";
 
 const Home = async () => {
-  const homePage = await getListPage("content/_index.md");
-  const { frontmatter } = homePage;
-  const { banner, feature, services, workflow, call_to_action } = frontmatter;
-  const { title } = config.site;
+  const agentPage = await getRegularPage("agent");
 
   return (
     <>
-      <SeoMeta title={title} />
-
-      {/* Banner */}
-      <HomeBanner banner={banner} />
-
-      {/* Features */}
-      <HomeFeatures feature={feature} />
-
-      {/* services */}
-      <Services services={services} />
-
-      {/* workflow */}
-      <Workflow workflow={workflow} />
-
-      {/* Cta */}
-      <Cta cta={call_to_action} />
+      <AgentChat data={agentPage} />
     </>
   );
 };
