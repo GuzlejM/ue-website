7ce11f7c756839f414ea2ecb7f5b1818b107a770
GuzlejM <martin.guzlej@easysoftware.com>
2025-01-20 14:37:23 +0100

    changing header footer navigation

--- components/Social.js
+++ components/Social.js
@@ -1,5 +1,7 @@
 "use client";
 
+import config from "@config/config.json";
+
 const Social = () => {
   const social = {
     facebook: "https://facebook.com/",
@@ -51,4 +53,4 @@ const Social = () => {
   );
 };
 
-export default Social;
+export default Social; 
--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -1,28 +1,31 @@
+"use client";
+
+import Logo from "@components/Logo";
 import Social from "@components/Social";
 import config from "@config/config.json";
 import menu from "@config/menu.json";
-import social from "@config/social.json";
-import { markdownify } from "@lib/utils/textConverter";
-import Image from "next/image";
 import Link from "next/link";
+import { markdownify } from "@lib/utils/textConverter";
 
 const Footer = () => {
-  const { copyright, footer_content } = config.params;
   const { footer } = menu;
+  const { logo } = config.site;
   return (
-    <footer className="section bg-theme-light pb-0">
+    <footer className="bg-[#1a1c3b] pt-24 pb-16">
       <div className="container">
-        {/* footer menu */}
         <div className="row">
           {footer.map((col) => {
             return (
               <div className="mb-12 sm:col-6 lg:col-3" key={col.name}>
-                {markdownify(col.name, "h2", "h4")}
-                <ul className="mt-6">
-                  {col?.menu.map((item) => (
-                    <li className="mb-1" key={item.text}>
-                      <Link href={item.url} rel="">
-                        {item.text}
+                {markdownify(col.name, "h2", "h4 text-white mb-8")}
+                <ul>
+                  {col?.children?.map((item) => (
+                    <li className="mb-4" key={item.name}>
+                      <Link 
+                        href={item.url} 
+                        className="text-gray-400 hover:text-white transition-colors"
+                      >
+                        {item.name}
                       </Link>
                     </li>
                   ))}
@@ -30,23 +33,20 @@ const Footer = () => {
               </div>
             );
           })}
-          {/* social icons */}
-          <div className="md-12 sm:col-6 lg:col-3">
-            <Link href="/" aria-label="Bigspring">
-              <Image
-                src={config.site.logo}
-                width={config.site.logo_width}
-                height={config.site.logo_height}
-                alt=""
-              />
+          <div className="md:col-6 lg:col-3">
+            <Link href="/" className="mb-8 inline-block">
+              <Logo src={logo} />
             </Link>
-            {markdownify(footer_content, "p", "mt-3 mb-6")}
-            <Social source={social} className="social-icons mb-8" />
+            <p className="text-gray-400 mb-8">
+              Transforming businesses with next-generation AI solutions and autonomous agents.
+            </p>
+            <Social />
           </div>
         </div>
-        {/* copyright */}
-        <div className="border-t border-border py-6">
-          {markdownify(copyright, "p", "text-sm text-center")}
+        <div className="border-t border-white/10 pt-8 mt-16">
+          <div className="text-center text-gray-400">
+            <p>© {new Date().getFullYear()} Bigspring. All rights reserved.</p>
+          </div>
         </div>
       </div>
     </footer>
@@ -54,3 +54,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
